f61bedbabc1ca4c1782cf1b20710db7c17290f42
Heather Smith <heathersmith@google.com>
2019-05-24 20:13:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,7 +18,10 @@ function sayHiToGrandma(string) {
   if (string.toLowerCase() === string) {
     return "I can't hear you!"
   }
-  if (string === "I love you, too") {
-    return "I love you, too"
+  if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
+  if (string.toUpperCase() === string) {
+    return "YES INDEED!"
   }
 }
